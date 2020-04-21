c727465852655b8f54f2e83832bed39655be06d2
vinayvnvv <tech.vinaybv@gmail.com>
2020-04-21 11:19:48 +0530

    sdsd

--- lib/env.dart
+++ lib/env.dart
@@ -1,6 +1,6 @@
 final environment = {
   'appVersion': '1.0',
-  'baseUrl': 'http://192.168.43.137:5000/api/'
+  // 'baseUrl': 'http://192.168.43.137:5000/api/'
   //  'baseUrl': 'https://ask-accion.herokuapp.com/api/',
-  //  'baseUrl': 'http://intranet.accionlabs.com:5000/api/',
+   'baseUrl': 'http://intranet.accionlabs.com:5000/api/',
 };
